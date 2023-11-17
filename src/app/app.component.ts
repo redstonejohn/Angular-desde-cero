0e829afafa195834aa556fe953cff26e2391b59e
Aira Dynecore <airadynecore@gmail.com>
2023-11-17 00:39:11 -0300

    el ultimo commit estuvo malo? revisar git vscode..

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+
+import { Product } from './models/product.model';
+
+
+import { HttpClient } from '@angular/common/http';
+
+import { ProductComponent } from "./components/product/product.component"
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, ProductComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
@@ -13,9 +21,17 @@ import { RouterOutlet } from '@angular/router';
 export class AppComponent {
   //Aqui van los valores que se quieran pasar al HTML (se ingresan en doble llaves {{ valor }})
   title = 'redstoneaira';
+  http = inject(HttpClient)
+  products: Product[] = []; //<--aqui
 
   changeTitle() {
     this.title = 'Kanae';
+  }
+
+  ngOnInit() {
+    this.http.get<Product[]>('https://api.escuelajs.co/api/v1/products').subscribe((data) => {
+      this.products = data;//<--aqui
+    })
   }
 }
 
