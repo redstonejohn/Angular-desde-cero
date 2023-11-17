057449684b25316ec7c64e1cda7fe630aef0afa0
Aira Dynecore <airadynecore@gmail.com>
2023-11-17 00:35:49 -0300

    Fetching Funcionando

--- src/app/components/product/product.component.ts
+++ src/app/components/product/product.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 @Component({
@@ -9,5 +9,7 @@ import { CommonModule } from '@angular/common';
   styleUrl: './product.component.css'
 })
 export class ProductComponent {
+  @Input() title: string;
+
 
 }
